d7d9dd13f2ec8af8c35f8130b185fcf555c0d3f5
Emanuel Benedict Ionescu <emanuel.ionescu27@gmail.com>
2025-09-24 10:06:37 +0300

    Manual Servo Control
    Signed-off-by: Emanuel Benedict Ionescu <emanuel.ionescu27@gmail.com>

--- src/manual_testing.py
+++ src/manual_testing.py
@@ -1,65 +1,95 @@
-import network
 import socket
 import time
 
-# WiFi
-ssid = ""
-password = ""
+try:
+    import ujson as json
+    RUNNING_ON = "PICO"
+except:
+    RUNNING_ON = "x86"
+    import json
 
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(ssid, password)
+with open("./config.json", 'r') as f:
+    PARAMETERS = json.load(f)
 
-max_wait = 10
-while max_wait > 0:
-    if wlan.status() < 0 or wlan.status() >= 3:
-        break
-    max_wait -= 1
-    print('waiting for connection...')
-    time.sleep(1)
 
-if wlan.status() != 3:
-    raise RuntimeError('network connection failed')
-else:
-    print('connected')
-    status = wlan.ifconfig()
-    print('ip = ' + status[0])
-
-# Server socket
-addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-s = socket.socket()
-s.bind(addr)
-s.listen(1)
-print('listening on', addr)
 
 def load_html():
     with open("./index.html", "r") as f:
         return f.read()
 
-while True:
-    try:
-        cl, addr = s.accept()
-        print('client connected from', addr)
-        request = cl.recv(1024).decode()
-        #print(request)
+def main():
+    
+    # WiFi
+    if RUNNING_ON == "PICO":
+        import network
 
-        stateis = None
+        ssid     = PARAMETERS["network"]["password"]
+        password = PARAMETERS["network"]["password"]
 
-        for i in range(1, 13):
-            if f"/slider{i}" in request:
-                pos = request.find("value=")
-                if pos != -1:
-                    val = request[pos+6:pos+9].split(" ")[0]
-                    stateis = (i - 1, val)
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+        wlan.connect(ssid, password)
 
-        print(stateis)
+        max_wait = 10
+        while max_wait > 0:
+            if wlan.status() < 0 or wlan.status() >= 3:
+                break
+            max_wait -= 1
+            print('waiting for connection...')
+            time.sleep(1)
 
-        response = load_html()
+        if wlan.status() != 3:
+            raise RuntimeError('network connection failed')
+        else:
+            print('connected')
+            status = wlan.ifconfig()
+            print('ip = ' + status[0])
 
-        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n'.encode())
-        cl.send(response.encode())
-        cl.close()
+    # Server socket
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+    s = socket.socket()
+    s.bind(addr)
+    s.listen(1)
+    print('listening on', addr)
 
-    except OSError as e:
-        cl.close()
-        print('connection closed')
+    while True:
+        try:
+            cl, addr = s.accept()
+            print('client connected from', addr)
+            request = cl.recv(1024).decode()
+            #print(request)
+
+            stateis = (-1, -1)
+
+            for i in range(0, 13):
+                if f"/input{i}" in request:
+                    pos = request.find("value=")
+                    if pos != -1:
+                        val = request[pos+6:pos+9].split(" ")[0]
+                        stateis = (i - 1, val)
+
+            print(stateis)
+            if stateis[0] == -1:
+                if stateis[1] == 0:
+                    #reset program
+                        return 0
+            else:
+                if RUNNING_ON == "PICO":
+                    pass
+
+
+            response = load_html()
+
+            cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n'.encode())
+            cl.send(response.encode())
+            cl.close()
+
+        except OSError as e:
+            cl.close()
+            print('connection closed')
+        except KeyboardInterrupt:
+            exit()
+
+if __name__ == "__main__":
+    while True:
+        main()
--- src/servo.py
+++ src/servo.py
@@ -1,22 +1,32 @@
 from machine import PWM
 
 class Servo:
-    def __init__(self, idx : int, pwm_pin : PWM,  min_percent : float, max_percent : float, positions : list[float]) -> None:
-        self.ID = idx
-        self.MAX = max_percent
-        self.MIN = min_percent
-        self.POS = 0 # in pencent
-        self.PWM_PIN = pwm_pin
-        self.POSITIONS = positions
+    def __init__(self, configuration : dict, PWM_PIN : PWM) -> None:
+        self.idx = configuration["id"]
+        self.max = configuration["max_percent"]
+        self.min = configuration["min_percent"]
+        self.pos = configuration["start_deg"]
+        self.pwm_pin = PWM_PIN
 
-        self.PWM_PIN.freq(50)
+        self.pwm_pin.freq(50)
+        
+        #power the servo 
+        self.set_degree(self.pos)
         
     def __get_duty_from_percent(self, percent : float) -> int:
+        percent = max(0, min(percent, 100))
         return int(percent/100 *2**16)
     
-    def set_position(self, pos : float) -> None:
-        self.PWM_PIN.duty_u16(self.__get_duty_from_percent(pos))
-        self.POS = pos
+    def __get_duty_from_degree(self, degree: int) -> int:
+        degree = max(-90, min(degree, 90))
+        percent = (degree + 90)/180 * (self.max - self.min) + self.min # scale the degree into operating interval
+        return self.__get_duty_from_percent(percent)
+
+    def set_position(self, pos_percent : float) -> None:
+        self.pwm_pin.duty_u16(self.__get_duty_from_percent(pos_percent))
         
-        
+    def set_degree(self, deg : int) -> None:
+        self.pwm_pin.duty_u16(self.__get_duty_from_degree(deg))
+        self.pos = deg    
+
     
